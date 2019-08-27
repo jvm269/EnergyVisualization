08e701f1ab554b78f3bcba94444330056fd44ab4
GanJin <gj2297@columbia.edu>
2019-08-26 22:11:49 -0400

    add stacked energy

--- static/js/app.js
+++ static/js/app.js
@@ -184,68 +184,80 @@ function buildMap(category, year) {
 };
 
 function buildStackedAreaChart(country) {
-  // var margin = {top: 10, right: 30, bottom: 30, left: 60};
-  // var width = 460 - margin.left - margin.right;
-  // var height = 400 - margin.top - margin.bottom;
-
-  // var svg = d3.select("#stacked-area-svg")
-  // .append("svg")
-  // .attr("width", width + margin.left + margin.right)
-  // .attr("height", height + margin.top + margin.bottom)
-  // .append("g")
-  // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // d3.json("/metadata/"+country, (data) => {
-  //   var sumstat = d3.nest()
-  //   .key(function(d) { return d.year;})
-  //   .entries(data);
-
-  //   console.log(sumstat)
-
-    // var mygroups = ["nuclear_electricity", "solar_electricity", "thermal_electricity", "tide_wave_and_ocean_electricity"];
-    // var mygroup = [1,2,3,4];
-    // var stackedData = d3.stack()
-    // .keys(mygroup)
-    // .value(function(d, key){
-    //   return d.values[key].quantity
-    // })
-    // (sumstat)
-
-    // console.log(stackedData)
-
+  var stacked = d3.select("#stacked-area-svg")
+  stacked.html("")
+  var margin = {top: 10, right: 30, bottom: 30, left: 60};
+  var width = 460 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
+  var svg = d3.select("#stacked-area-svg")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var myUrl = `/metadata/${country}`;
+  d3.json(myUrl, (data) => {
+    var sumstat = d3.nest()
+    .key(function(d) { return +d.year;})
+    .entries(data);
+  // var myUrl = `/sample_energy/${country}`;
+  // d3.json(myUrl).then(function(response) {
+    console.log(data)
+    // response.forEach(function(datalvl) {
+    //   datalvl.poverty = +datalvl.poverty;
+    //   datalvl.age = +datalvl.age;
+    //   datalvl.smokes = +datalvl.smokes;
+    // });
+    // d3.json('/sample/${country}', (data) => {
+    // var sumstat = d3.nest()
+    // .key(function(d) { return d.year;})
+    // .entries(data);
+    // console.log(sumstat)
+    var mygroups = ["nuclear_electricity", "solar_electricity", "thermal_electricity", "tide_wave_and_ocean_electricity"];
+    var mygroup = [1,2,3,4];
+    var stackedData = d3.stack()
+    .keys(mygroup)
+    .value(function(d, key){
+      // console.log(d.values[key])
+      console.log(key)
+      if (`${key-1}` in d.values){
+        return +d.values[key-1].quantity
+      } else{
+        return 0 
+      }
+    })
+    (sumstat)
+    console.log(stackedData)  
     // Add X axis --> it is a date format
-  // var x = d3.scaleLinear()
-  // .domain(d3.extent(data, function(d) { return d.year; }))
-  // .range([ 0, width ]);
-  // svg.append("g")
-  //   .attr("transform", "translate(0," + height + ")")
-  //   .call(d3.axisBottom(x).ticks(5));
-
-  // // Add Y axis
-  // var y = d3.scaleLinear()
-  //   .domain([0, d3.max(data, function(d) { return +d.n; })*1.2])
-  //   .range([ height, 0 ]);
-  // svg.append("g")
-  //   .call(d3.axisLeft(y));
-
-// // color palette
-// var color = d3.scaleOrdinal()
-//   .domain(mygroups)
-//   .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
-
-// // Show the areas
-// svg
-//   .selectAll("mylayers")
-//   .data(stackedData)
-//   .enter()
-//   .append("path")
-//     .style("fill", function(d) { name = mygroups[d.key-1] ;  return color(name); })
-//     .attr("d", d3.area()
-//       .x(function(d, i) { return x(d.data.key); })
-//       .y0(function(d) { return y(d[0]); })
-//       .y1(function(d) { return y(d[1]); })
-//   )
-  // });
+  var x = d3.scaleLinear()
+  .domain(d3.extent(data, function(d) { return +d.year; }))
+  .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height +")")
+    .call(d3.axisBottom(x));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0,1.3* d3.max(data, function(d) { return +d.quantity; })*1.2])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+// color palette
+var color = d3.scaleOrdinal()
+  .domain(mygroups)
+  .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+// Show the areas
+svg
+  .selectAll("mylayers")
+  .data(stackedData)
+  .enter()
+  .append("path")
+    .style("fill", function(d) { name = mygroups[d.key-1] ;  return color(name); })
+    .attr("d", d3.area()
+      .x(function(d, i) { return x(d.data.key); })
+      .y0(function(d) { return y(d[0]); })
+      .y1(function(d) { return y(d[1]); })
+    )
+  });
 }
 
 function pie_chart(country, year) {
